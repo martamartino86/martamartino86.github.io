0b6543ee348a25979246fdcb91a1589a15a59c25
Marta <martamartino86@gmail.com>
2020-04-19 23:06:12 +0200

    Commit for web

---
+++ www/script.js
@@ -0,0 +1,11 @@
+﻿
+function showPopup() {
+	var popup = document.getElementById('popup')	
+	popup.classList.add('show')
+}
+
+
+function hidePopup() {
+	var popup = document.getElementById('popup')	
+	popup.classList.remove('show')
+}
